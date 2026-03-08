1f83f5597e451ffe73c79bfb2ab93018ebf461d8
imperatorpavel205-sudo <imperatorpavel205@gmail.com>
2026-03-08 18:19:17 +0300

    Add production settings and Render configuration

--- staticfiles/wsgi.py
+++ staticfiles/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for therapy project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therapy.settings')
+if os.environ.get('RENDER', False):
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therapy.settings_production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therapy.settings')
 
 application = get_wsgi_application()
